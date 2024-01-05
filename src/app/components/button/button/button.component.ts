009c4b4a0b98f2913af896042b594f7276d0a454
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-04 21:56:43 -0400

    [FIX]selected styles

--- src/app/components/button/button/button.component.ts
+++ src/app/components/button/button/button.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
 })
 export class ButtonComponent {
   @Input() type : 'button' | 'reset' | 'submit' = 'button'
-  @Input() bg : string = 'red'
+  @Input() bg : string = ''
+
+  getColors(){
+    return{
+      'bg-success' : this.bg === 'success',
+      'bg-blue-400' : this.bg === ''
+    }
+  }
 }
