17cdc9d77fd58fbaad42b9e1466eb9cff93823ad
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-05 23:07:22 -0400

    [FEAT]added overlay navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, OverlayModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
-
+  isOpen = false;
 }
