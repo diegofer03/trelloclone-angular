0ad4787600ca62698d5f3416726b5cf330f4e892
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-06 23:14:56 -0400

    [FEAT]added font awesome icons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import {OverlayModule} from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ButtonComponent, OverlayModule],
+  imports: [ButtonComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  faBell = faBell
+  faInfoCircle = faInfoCircle
   isOpen = false;
 }
