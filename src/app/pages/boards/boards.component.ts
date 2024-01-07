521b135dd68288c02dd50ae823a5ec398267487b
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-07 19:55:02 -0400

    [FEAT]added boards view

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { faBox, faClock, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
-
+  faTrello = faTrello
+  faBox = faBox
+  faWaveSqueare = faWaveSquare
+  faClock = faClock
 }
