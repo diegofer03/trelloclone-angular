a841adc1caca7adb37689a254ff34506e2d2d24e
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-07 21:37:49 -0400

    [FEAT]added accordion cdk

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
-import { faBox, faClock, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faBorderAll, faBox, faClock, faGear, faHeart, faUsers, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -15,4 +16,10 @@ export class BoardsComponent {
   faBox = faBox
   faWaveSqueare = faWaveSquare
   faClock = faClock
+  faAngleDown = faAngleDown
+  faAngleUp = faAngleUp
+  faBorderAll = faBorderAll
+  faGear = faGear
+  faHeart = faHeart
+  faUsers = faUsers
 }
