6525b1931ab75adca46c69a71052baa75619f4d4
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-17 17:31:32 -0400

    [FIX]login funcionality

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { faEye, faEyeSlash, faPen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonComponent, RouterLink],
+  imports: [ButtonComponent, RouterLink, ReactiveFormsModule, FontAwesomeModule, CommonModule],
   templateUrl: './login.component.html'
 })
 export default class LoginComponent {
+  formBuilder = inject(FormBuilder)
+  router = inject(Router)
+
+  form = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [ Validators.required, Validators.minLength(6)]],
+  });
+
+  faPen = faPen;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+  showPassword = false;
+  status: string = 'init';
+
+  constructor(
+  ) { }
+
+  doLogin() {
+    if (this.form.valid) {
+      this.status = 'loading';
+      const { email, password } = this.form.getRawValue();
+      console.log(email)
+      // TODO
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 
 }
