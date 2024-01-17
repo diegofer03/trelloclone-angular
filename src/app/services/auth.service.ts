9a218e81110218a8e2d2fe9fc654ea62453392ca
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-17 17:35:20 -0400

    [FEAT]added auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+  http = inject(HttpClient)
+  apiUrl = enviroment.API_URL
+  constructor() { }
+
+  login(email: string, pass: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
+      email,
+      pass
+    });
+  }
+}
