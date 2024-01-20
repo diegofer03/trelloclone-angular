c03fb10669c76b3997794126b30c221c95efcc75
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-19 20:32:37 -0400

    [FEAT]added routes in anchors

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './sign-up.component.html',
 })
 export class SignupComponent {
