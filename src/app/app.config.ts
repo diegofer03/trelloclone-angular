038fef4769230be1ae87e50e8a6ce86c6904ed1c
Diego Fernandez <diegoafanador10@gmail.com>
2024-01-27 13:53:15 -0400

    [FEAT]added valid token interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
 import { errorsInterceptor } from './interceptors/errors.interceptor';
+import { validTokenInterceptor } from './interceptors/valid-token.interceptor';
 // provideHttpClient(withFetch())
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes,
     withPreloading(PreloadAllModules)),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([tokenInterceptor, errorsInterceptor]))]
+    provideHttpClient(withInterceptors([tokenInterceptor, errorsInterceptor, validTokenInterceptor]))]
 };
---
+++ src/app/interceptors/valid-token.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { SessionService } from '../services/session.service';
+import { Router } from '@angular/router';
+
+export const validTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(SessionService).isValid();
+  const router = inject(Router);
+  if(!token){
+    router.navigate(['/login']);
+    return next(req);
+  }
+
+  return next(req);
+};
